d465f9f80eb88695b998f3f8132ec169c4c0d788
zhangruinan <ruinan.zhang@icloud.com>
2020-07-07 18:51:45 +0800

    添加 search cf, extend node, search node

--- kgexplore/__init__.py
+++ kgexplore/__init__.py
@@ -1,4 +1,4 @@
 from .configurations import config
 from .visual import visualize
 from .client import search_bigram
-from .client import get_xiushang_ngram_related,get_xiushang_ngram,get_xiushang_edge,get_xiushang_node
+from .client import get_xiushang_ngram_related,get_xiushang_ngram,get_xiushang_edge,get_xiushang_node,search_ngram_node,extend_node,search_ngram_cf
